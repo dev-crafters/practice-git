5df5d1a3e372714f7e4db15d09eda8c931fa07c5
STYersonH <102554781+STYersonH@users.noreply.github.com>
2024-09-11 17:08:31 -0500

    feat: add an space in the second line  (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <>Practicando con GIT</>;
+  return (
+    <>
+      <h1>Practicando con GIT</h1>
+      <p>Este es el nuevo cambio </p>
+    </>
+  );
 }
 
 export default App;
