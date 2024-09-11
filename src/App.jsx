9dadd7d929d17ffb1c759177d92e346f6c2a4abd
STYersonH <102554781+STYersonH@users.noreply.github.com>
2024-09-11 17:16:46 -0500

    feat: agregar un div con contenido (#4)
    la descripcion para esta feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ function App() {
     <>
       <h1>Practicando con GIT</h1>
       <p>Este es el nuevo cambio </p>
+      <div>
+        <p>Tenemos un nuevo commit</p>
+        <p>
+          En este caso estamos creando un cambio sin haber creado una rama en
+          github
+        </p>
+      </div>
     </>
   );
 }
