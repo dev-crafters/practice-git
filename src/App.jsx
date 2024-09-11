0f0d0f441b38c98a720c0a993e433367dceb51a3
STYersonH <102554781+STYersonH@users.noreply.github.com>
2024-09-11 17:18:37 -0500

    feat: Agregar un div con contenido referenciando issue
    En este caso estamos referenciando un issue para lo cual colocamos su ID
    
    resolves: #2

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ function App() {
           github
         </p>
       </div>
+
+      <div>
+        <p>Tenemos otro nuevo commit pero ahora referenciando el issue</p>
+        <p>En este caso referenciaremos el issue al crear el commit</p>
+      </div>
     </>
   );
 }
