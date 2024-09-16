8b14d5aced4d2d6f40abb341102a325d853fdf31
STYersonH <102554781+STYersonH@users.noreply.github.com>
2024-09-16 10:27:31 -0500

    Agregando nuevo div (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ function App() {
           github
         </p>
       </div>
+      <div>
+        <p>Tenemos otro nuevo commit pero ahora referenciando el issue</p>
+        <p>En este caso referenciaremos el issue al crear el commit</p>
+        <p>creo que no se subio antes</p>
+      </div>
     </>
   );
 }
